1bd19ffe9611d03e6d5be606d35910749e1ff1e3
SE-Designs <64894804+SE-Designs@users.noreply.github.com>
2022-07-11 17:38:40 +0600

    mobile menu

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// MOBILE MENU:
+function toggleMobileMenu() {
+  var x = document.getElementById("mobile-nav");
+  var proper = window.getComputedStyle(x, null);
+  var y = document.getElementById("burger");
+  if (proper.display === "none"){
+    x.style.display = "flex";
+    y.classList.add("burger-active");
+    document.getElementById("hero").style.display = "none"
+    document.getElementById("main").style.display = "none"
+    document.getElementById("footer").style.display = "none"
+  } else {
+    x.style.display = "none";
+    y.classList.remove("burger-active");
+    document.getElementById("hero").style.display = "block"
+    document.getElementById("main").style.display = "block"
+    document.getElementById("footer").style.display = "block"
+  }
+} 
 
 // FAQ:
 let answers = document.querySelectorAll(".faq-block");
