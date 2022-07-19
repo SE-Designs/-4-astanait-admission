d277be68d7a78c3b35fad1482ae42e4d9c510907
SE-Designs <64894804+SE-Designs@users.noreply.github.com>
2022-07-19 20:38:45 +0600

    minor fixes

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // MOBILE MENU:
 function toggleMobileMenu() {
-  var x = document.getElementById("mobile-nav");
-  var proper = window.getComputedStyle(x, null);
-  var y = document.getElementById("burger");
-  if (proper.display === "none"){
-    x.style.display = "flex";
-    y.classList.add("burger-active");
+  let mobileNav = document.getElementById("mobile-nav");
+  let proper = window.getComputedStyle(mobileNav, null);
+  let burgerMenu = document.getElementById("burger");
+  if (proper.display === "none") {
+    document.getElementById("mobile-nav").style.display = "flex";
+    burgerMenu.classList.add("burger-active");
     document.getElementById("hero").style.display = "none"
     document.getElementById("after-hero").style.display = "none"
     document.getElementById("main").style.display = "none"
     document.getElementById("footer").style.display = "none"
   } else {
-    x.style.display = "none";
-    y.classList.remove("burger-active");
+    document.getElementById("mobile-nav").style.display = "none";
+    burgerMenu.classList.remove("burger-active");
     document.getElementById("hero").style.display = "block"
-    document.getElementById("after-hero").style.display = "none"
+    document.getElementById("after-hero").style.display = "block"
     document.getElementById("main").style.display = "block"
     document.getElementById("footer").style.display = "block"
   }
